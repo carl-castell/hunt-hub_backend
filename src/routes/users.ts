36fa9d27c3484844954c4af6288a2d0bb239a615
carl-castell <118299961+carl-castell@users.noreply.github.com>
2026-04-13 12:03:17 +0200

    added clear all tables command for db

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -8,7 +8,7 @@ const router = Router();
 // List of x users
 router.get('/', async (req: Request, res: Response) => {
   const users = await db.query.usersTable.findMany({
-    limit: 2,
+    limit: 15,
   });
   res.render('users', { users, title: 'Users List' });
 });
